b76fd8d4fd82f22d089dbb8b28ccd4e0d260b2e0
cbmja <cbmja@naver.com>
2024-05-08 12:59:35 +0900

    .0508

--- src/main/java/com/daishin/pdf/controller/PostalCreationRequestController.java
+++ src/main/java/com/daishin/pdf/controller/PostalCreationRequestController.java
@@ -18,8 +18,6 @@ public class PostalCreationRequestController {
         private final ReqSaveService reqSaveService;
 
 
-    ///////////////
-
     @PostMapping("/upload")
     @ResponseBody
     public Map<String , String> uploadAndUnzip(@RequestParam(name = "File" , required = false) MultipartFile File , @ModelAttribute ReqParam req) throws IOException {
@@ -27,38 +25,15 @@ public class PostalCreationRequestController {
         Map<String , String> response = new LinkedHashMap<>();
 
         //파일저장처리
-        
+        Common.savePdf(File , req);
         
         //db저장
         reqSaveService.save(req);
-
-
 
         response.put("결과" , "수신 완료 등등");
         return response;
 
     }
-
-/////////////////////////////
-    /*
-
-        {
-        "master" :{
-        "TR_KEY":"111-111"
-        ,"TOTAL_SEND_CNT":"3"
-        ,"PDF_YN":"N"
-        ,"DLV_TYPE_CD":"일반"
-        ,"PRINT_TYPE_NM":"안내장"
-        ,"PAGE_CNT":"78"
-        ,"RET_YN":"N"},
-        "detail":[
-        {"RECV_NUM" : "1" , "DM_LINK_KEY" : "12-1"  , "DLV_CD" : "집중국코드^|집중국명^|배달국코드^|배달국명^|집배팅코드^|집배구코드^|집배인증용" , "RECV_NM" : "홍길동" , "RECV_POST_CD" : "122-2" , "RECV_ADDR" : "서울시 노원구333" , "RECV_ADDR_DETAIL" : "531-2903"}
-        , {"RECV_NUM" : "2" , "DM_LINK_KEY" : "12-2"  , "DLV_CD" : "집중국코드^|집중국명^|배달국코드^|배달국명^|집배팅코드^|집배구코드^|집배인증용" , "RECV_NM" : "홍이동" , "RECV_POST_CD" : "102-42" , "RECV_ADDR" : "서울시 강남구444" , "RECV_ADDR_DETAIL" : "212-5303"}
-        , {"RECV_NUM" : "3" , "DM_LINK_KEY" : "12-3"  , "DLV_CD" : "집중국코드^|집중국명^|배달국코드^|배달국명^|집배팅코드^|집배구코드^|집배인증용" , "RECV_NM" : "홍삼동" , "RECV_POST_CD" : "213-2" , "RECV_ADDR" : "인천 중구555" , "RECV_ADDR_DETAIL" : "203-5323"}
-        ]
-        }
-
-    */
 
 
 
--- src/main/java/com/daishin/pdf/util/Common.java
+++ src/main/java/com/daishin/pdf/util/Common.java
@@ -1,6 +1,8 @@
 package com.daishin.pdf.util;
 
 import com.daishin.pdf.dto.ReqParam;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
@@ -11,97 +13,48 @@ import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+@Component
 public final class Common {
+
+
 
     /**
      * 파일 저장 일시 포맷
      * @return
      */
-    public static String getCurrnetYYYYMM() {
-        SimpleDateFormat sdfyyyymm = new SimpleDateFormat("yyyyMM");
+    public static String getDate() {
+        SimpleDateFormat sdfyyyymm = new SimpleDateFormat("yyyyMM_HHmmss");
         Date now = new Date();
         return sdfyyyymm.format(now);
     }
 
-
-public static void savePdf(MultipartFile file , ReqParam reqParam){
-
-    String dir = "C:\\DATA\\"+reqParam.getTR_KEY()+"\\"+getCurrnetYYYYMM()+"\\";
-
-
-
-
-}
-
-
-
-    //////////////////////파일 이동 SSSSSSSSSS //////////////////////
     /**
-     * path1 에 있는 파일들을 path2로 옮김
-     * @param path1
-     * @param path2
+     * pdf 저장
+     * @param file
+     * @param reqParam
      */
-    public static void moveFile(String path1 , String path2){
-        Path sourceDir = Paths.get(path1);
-        Path targetDir = Paths.get(path2);
+    public static void savePdf(MultipartFile file , ReqParam reqParam){
 
+
+
+        //저장될 pdf 파일명
+        String fileName = getDate()+"_"+file.getOriginalFilename();
+        //파일 저장 경로
+        String path = "C:\\\\DATA\\\\"+reqParam.getTR_KEY()+"\\";
+    
+        //디렉토리 생성
+        File dir = new File(path);
+        dir.mkdirs();
+    
+    
+        //파일 저장처리SSS
+        Path pdfPath = Paths.get(path).resolve(fileName);
         try {
-            // 대상 디렉토리 존재 확인 및 생성
-            if (!Files.exists(targetDir)) {
-                Files.createDirectories(targetDir);
-                System.out.println("대상 디렉토리 생성: " + targetDir);
-            }
-
-            // 원본 디렉토리에서 모든 파일을 반복하여 처리
-            Files.list(sourceDir).forEach(sourcePath -> {
-                try {
-                    // 대상 디렉토리 경로 생성
-                    Path targetPath = targetDir.resolve(sourcePath.getFileName());
-
-                    // 파일 존재 확인
-                    if (Files.exists(sourcePath)) {
-                        // 파일 이동
-                        Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
-                        System.out.println("파일 이동: " + sourcePath + " -> " + targetPath);
-                    } else {
-                        System.out.println("이동할 파일이 존재하지 않습니다: " + sourcePath);
-                    }
-                } catch (IOException e) {
-                    System.err.println("파일 이동 중 에러 발생: " + sourcePath);
-                    e.printStackTrace();
-                }
-            });
-        } catch (IOException e) {
-            System.err.println("디렉토리 읽기 중 에러 발생");
+            file.transferTo(pdfPath.toFile());
+        } catch (IllegalStateException | IOException e) {
             e.printStackTrace();
         }
+    
     }
-//////////////////////파일 이동 EEEEEEEEEE //////////////////////
 
-    ////+++++++++++++++++++++//////////////+++++++++++++++++++++///////////////////////////+++++++++++++++++++++////////////////////
-/*
-
-    public static ReqParam mapToObj(Map req){
-        ReqParam reqParam = new ReqParam();
-        reqParam.setApiKey((String)req.get("apiKey"));
-        reqParam.setTR_KEY((String)req.get("TR_KEY"));
-        reqParam.setTOTAL_SEND_CNT((String)req.get("TOTAL_SEND_CNT"));
-        reqParam.setDLV_TYPE_CD((String)req.get("DLV_TYPE_CD"));
-        reqParam.setPRINT_TYPE_NM((String)req.get("PRINT_TYPE_NM"));
-        reqParam.setPAGE_CNT((String)req.get("PAGE_CNT"));
-        reqParam.setRET_YN((String)req.get("RET_YN"));
-        reqParam.setRECV_NUM((String)req.get("RECV_NUM"));
-        reqParam.setDM_LINK_KEY((String)req.get("DM_LINK_KEY"));
-        reqParam.setPDF_NM((String)req.get("PDF_NM"));
-        reqParam.setDLV_CD((String)req.get("DLV_CD"));
-        reqParam.setRECV_NM((String)req.get("RECV_NM"));
-        reqParam.setRECV_POST_CD((String)req.get("RECV_POST_CD"));
-        reqParam.setRECV_ADDR((String)req.get("RECV_ADDR"));
-        reqParam.setRECV_ADDR_DETAIL((String)req.get("RECV_ADDR_DETAIL"));
-
-        return reqParam;
-    }
-*/
-
-    ////+++++++++++++++++++++//////////////+++++++++++++++++++++///////////////////////////+++++++++++++++++++++////////////////////
 }
