4a068ab61552d1860a360f0c2720565996efdb73
cbmja <cbmja@naver.com>
2024-07-03 15:48:36 +0900

    backup_role_back_here

--- src/main/java/com/daishin/pdf/SchedulerConfig/SchedulerConfiguration.java
+++ src/main/java/com/daishin/pdf/SchedulerConfig/SchedulerConfiguration.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
-
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
