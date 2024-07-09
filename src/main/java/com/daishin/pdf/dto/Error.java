3ad0a03aab3b99bf2033bd696e8748bc1d2022e3
cbmja <cbmja@naver.com>
2024-07-09 11:09:20 +0900

    .

--- src/main/java/com/daishin/pdf/dto/Error.java
+++ src/main/java/com/daishin/pdf/dto/Error.java
@@ -7,15 +7,10 @@ import java.time.LocalDateTime;
 @Data
 public class Error {
 
-    @JsonProperty("NUM")
     private int NUM;
-    @JsonProperty("ERROR_MESSAGE")
     private String ERROR_MESSAGE;
-    @JsonProperty("CREATE_DATE")
     private LocalDateTime CREATE_DATE;
-    @JsonProperty("REPOSITORY")
     private String REPOSITORY;
-    @JsonProperty("MASTER_KEY")
     private String MASTER_KEY;
 
 }
