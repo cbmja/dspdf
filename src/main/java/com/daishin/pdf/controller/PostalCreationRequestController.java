5560188f01cd6f8fa755fc7fc1025a0fc631d147
cbmja <cbmja@naver.com>
2024-07-15 16:48:16 +0900

    수신중인 경우 중복전송시 업데이트 ok

--- src/main/java/com/daishin/pdf/controller/PostalCreationRequestController.java
+++ src/main/java/com/daishin/pdf/controller/PostalCreationRequestController.java
@@ -14,7 +14,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
